e83b13d632a99ed733727a5e85e5761ef2935837
Austin Bruso <austin.bruso@hotmail.com>
2022-04-08 16:59:38 -0500

    Added Ticket Status, Ticket Priority, and Ticket Type Classes

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTrackerProject.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Status Name")]
+        public string Name { get; set; }
+
+    }
+}
