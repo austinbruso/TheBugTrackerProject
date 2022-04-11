1161632907b9a9bd26ad3786a4861de207c54f8e
Austin Bruso <austin.bruso@hotmail.com>
2022-04-11 04:30:59 -0500

    Added CompanyInfo Interface

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,18 @@
+﻿using BugTrackerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTrackerProject.Models;
+
+namespace TheBugTrackerProject.Services.Interfaces
+{
+   public interface IBTCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoById(int? companyId);
+        public Task<BTUser> GetAllMembersAsync(int companyId);
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId);
+
+    }
+}
