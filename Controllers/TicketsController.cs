bb39c91687b6bd3b2614d0522f64b81a00f88178
Austin Bruso <austin.bruso@hotmail.com>
2022-05-09 17:57:44 -0500

    Added MyTickets Feature

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -39,6 +39,18 @@ namespace TheBugTrackerProject.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+
+        //Get: My Tickets 
+
+        public async Task<IActionResult> MyTickets()
+        {
+            BTUser btUser = await _userManager.GetUserAsync(User);
+
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(btUser.Id, btUser.CompanyId.Value);
+
+            return View(tickets);
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
